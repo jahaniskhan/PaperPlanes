c89ac23d0b86b44d6d0beb5accd6739200d6f5f9
jahaniskhan <74698323+jahaniskhan@users.noreply.github.com>
2023-12-14 23:51:34 -0800

    Revert "flattened simulation"

--- src/Behaviors.hs
+++ src/Behaviors.hs
@@ -8,44 +8,49 @@ import Graphics.Gloss.Data.Color
 import Debug.Trace (trace)
 
 hover :: Entity -> Entity
-hover (Entity pos _ _ color) = Entity pos (Velocity 0 0) (Acceleration 0 0) color
+hover (Entity pos _ _ color) = Entity pos (Velocity 0 0 0) (Acceleration 0 0 0) color
 
 moveLinearly :: Velocity -> Entity -> Entity 
-moveLinearly (Velocity vx vy) (Entity pos _ _ color) = Entity pos (Velocity vx vy) (Acceleration 0 0) color
+moveLinearly (Velocity vx vy vz) (Entity pos _ _ color) = Entity pos (Velocity vx vy vz) (Acceleration 0 0 0) color
 
 accelerate :: Acceleration -> Entity -> Entity 
 accelerate acc (Entity pos vel _ color) = Entity pos vel acc color
 
 turn :: Float -> Entity -> Entity
-turn angle (Entity pos (Velocity vx vy) acc color) = 
+turn angle (Entity pos (Velocity vx vy vz) acc color) = 
     Entity pos (Velocity (vx * P.cos (realToFrac angle) - vy * P.sin (realToFrac angle)) 
-                         (vx * P.sin (realToFrac angle) + vy * P.cos (realToFrac angle))) acc color
+                         (vx * P.sin (realToFrac angle) + vy * P.cos (realToFrac angle)) vz) acc color
 
 chase :: Entity -> Entity -> Entity 
-chase drone@(Entity (Position dx dy) (Velocity vx vy) _ droneColor) target@(Entity (Position tx ty) _ _ _) = 
+chase drone@(Entity (Position dx dy dz) (Velocity vx vy vz) _ droneColor) target@(Entity (Position tx ty tz) _ _ _) = 
     let
         directionX = tx - dx
         directionY = ty - dy
+        directionZ = tz - dz
 
-        distance = sqrt (directionX^2 + directionY^2)
-        (normDirectionX, normDirectionY) = if distance == 0
-            then (0, 0)
-            else (directionX / distance, directionY / distance)
+        distance = sqrt (directionX^2 + directionY^2 + directionZ^2)
+        --Avoid division by zero whih can cause NaN
+        (normDirectionX, normDirectionY,normDirectionZ) = if distance == 0
+            then (0,0,0)
+            else (directionX / distance, directionY / distance, directionZ / distance)
         chaseSpeed = min 1 (distance / 10)
         newVelX = vx + normDirectionX * chaseSpeed * 0.1
         newVelY = vy + normDirectionY * chaseSpeed * 0.1
+        newVelZ = vz + normDirectionZ * chaseSpeed * 0.1
 
-    in Entity (Position dx dy) (Velocity newVelX newVelY) (Acceleration 0 0) droneColor
+    in trace ("chase: drone= " ++ show drone ++ "target= " ++ show target ++ ", newVel=(" ++ show newVelX ++ ", " ++ show newVelY ++ ", " ++ show newVelZ ++ ")") 
+        Entity (Position dx dy dz) (Velocity newVelX newVelY newVelZ) (Acceleration 0 0 0) droneColor
+
 
 -- because position velocity and color are defined
 initialPosition :: Position 
-initialPosition = Position 0 0
+initialPosition = Position 0 0 0
 
 initialVelocity :: Velocity
-initialVelocity = Velocity 0 0
+initialVelocity = Velocity 0 0 0
 
 initialAcceleration :: Acceleration
-initialAcceleration = Acceleration 0 0
+initialAcceleration = Acceleration 0 0 0
 
 
 
@@ -54,10 +59,11 @@ initialErraticTarget = Entity initialPosition initialVelocity initialAcceleratio
 -- this 
 
 erraticMove :: Float -> Entity -> Entity 
-erraticMove time (Entity pos (Velocity vx vy) acc color) = 
+erraticMove time (Entity pos (Velocity vx vy vz) acc color) = 
     let 
         newVelX = vx + (P.sin (realToFrac time)) -- sinsoidal variation because our target is lazy
         newVelY = vy + (P.cos (realToFrac time))
-    in Entity pos (Velocity newVelX newVelY) acc color
+        newVelZ  = vz
+    in Entity pos (Velocity newVelX newVelY newVelZ) acc color
 
 
